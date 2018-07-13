d1a6f72067b1a3f6f37addf261c74c91d36356b1
HossamHassan47 <hossam.hassan@outlook.com>
2018-07-13 14:21:07 +0200

    content provider

--- app/src/main/java/com/wordpress/hossamhassan47/inventoryapp/data/InventoryProvider.java
+++ app/src/main/java/com/wordpress/hossamhassan47/inventoryapp/data/InventoryProvider.java
@@ -122,6 +122,24 @@ public class InventoryProvider extends ContentProvider {
      * for that specific row in the database.
      */
     private Uri insertProduct(Uri uri, ContentValues values) {
+        // Check the name is not null
+        String name = values.getAsString(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        if (name == null) {
+            throw new IllegalArgumentException("Product requires a name");
+        }
+
+        // If the price is provided, check that it's greater than or equal to 0
+        Integer price = values.getAsInteger(InventoryContract.ProductEntry.COLUMN_PRICE);
+        if (price == null || price < 0) {
+            throw new IllegalArgumentException("Product requires valid price");
+        }
+
+        // If the quantity is provided, check that it's greater than or equal to 0
+        Integer quantity = values.getAsInteger(InventoryContract.ProductEntry.COLUMN_QUANTITY);
+        if (quantity == null || quantity < 0) {
+            throw new IllegalArgumentException("Product requires valid quantity");
+        }
+
         // Gets the database in write mode
         SQLiteDatabase db = mDbHelper.getWritableDatabase();
 
@@ -152,6 +170,67 @@ public class InventoryProvider extends ContentProvider {
      */
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
-        return 0;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PRODUCTS:
+                return updateProduct(uri, values, selection, selectionArgs);
+            case PRODUCT_ID:
+                // For the PRODUCT_ID code, extract out the ID from the URI,
+                // so we know which row to update. Selection will be "_id=?" and selection
+                // arguments will be a String array containing the actual ID.
+                selection = InventoryContract.ProductEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+
+                return updateProduct(uri, values, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Update is not supported for " + uri);
+        }
+    }
+
+    /**
+     * Update products in the database with the given content values. Apply the changes to the rows
+     * specified in the selection and selection arguments (which could be 0 or 1 or more products).
+     * Return the number of rows that were successfully updated.
+     */
+    private int updateProduct(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        // If there are no values to update, then don't try to update the database
+        if (values.size() == 0) {
+            return 0;
+        }
+
+        // If the {@link ProductEntry#COLUMN_PRODUCT_NAME} key is present,
+        // check that the name value is not null.
+        if (values.containsKey(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME)) {
+            String name = values.getAsString(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME);
+            if (name == null) {
+                throw new IllegalArgumentException("Product requires a name");
+            }
+        }
+
+        // If the {@link ProductEntry#COLUMN_PRICE} key is present,
+        // check that the price value is valid.
+        if (values.containsKey(InventoryContract.ProductEntry.COLUMN_PRICE)) {
+            // Check that the price is greater than or equal to 0 kg
+            Integer price = values.getAsInteger(InventoryContract.ProductEntry.COLUMN_PRICE);
+            if (price == null || price < 0) {
+                throw new IllegalArgumentException("Product requires valid price");
+            }
+        }
+
+        // If the {@link ProductEntry#COLUMN_QUANTITY} key is present,
+        // check that the quantity value is valid.
+        if (values.containsKey(InventoryContract.ProductEntry.COLUMN_QUANTITY)) {
+            // Check that the quantity is greater than or equal to 0 kg
+            Integer quantity = values.getAsInteger(InventoryContract.ProductEntry.COLUMN_QUANTITY);
+            if (quantity == null || quantity < 0) {
+                throw new IllegalArgumentException("Product requires valid quantity");
+            }
+        }
+
+        // Otherwise, get writable database to update the data
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
+
+        // Returns the number of database rows affected by the update statement
+        return database.update(InventoryContract.ProductEntry.TABLE_NAME, values, selection, selectionArgs);
     }
 }
